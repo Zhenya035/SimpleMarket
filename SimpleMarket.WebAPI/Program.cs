538a76adec27e6ebfb75548af057ebf863741d41
Yauheni Shumsky <shumsky.yauheni@gmail.com>
2025-03-05 23:27:28 +0300

    connect Database

--- SimpleMarket.WebAPI/Program.cs
+++ SimpleMarket.WebAPI/Program.cs
@@ -1,6 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using SimpleMarket.Persistance;
+
 var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<SimpleMarketDbContext>(
+    options =>
+    {
+        options.UseMySql(configuration.GetConnectionString(nameof(SimpleMarketDbContext)),
+            new MySqlServerVersion(new Version(8, 0, 36)));
+    }
+    );
+
 var app = builder.Build();
 
+app.UseSwagger();
+app.UseSwaggerUI();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
