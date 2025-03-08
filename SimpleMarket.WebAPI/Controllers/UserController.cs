0a84876f10168e178dcebdd5baea42ab86c068a9
Yauheni Shumsky <shumsky.yauheni@gmail.com>
2025-03-08 21:55:44 +0300

    add controller

---
+++ SimpleMarket.WebAPI/Controllers/UserController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleMarket.Application.DTOs;
+using SimpleMarket.Application.Services;
+using SimpleMarket.Core.Models;
+
+namespace SimpleMarket.WebAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController(UserService userService) : ControllerBase
+{
+    [HttpGet("users")]
+    public async Task<ActionResult<List<User>>> Get()
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+        
+        try
+        {
+            var users = await userService.GetAllUsers();
+            return Ok(users);
+        }
+        catch (Exception e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+
+    [HttpGet("users/{id}")]
+    public async Task<ActionResult<User>> Get(long id)
+    {
+        try
+        {
+            var user = await userService.GetUserById(id);
+            return Ok(user);
+        }
+        catch (Exception e)
+        {
+            return NotFound(e.Message);
+        }
+    }
+
+    [HttpPost("users/add")]
+    public async Task<IActionResult> Add([FromBody] AddUserRequest newUser)
+    {
+        var user = new User
+        {
+            Role = newUser.Role,
+            Username = newUser.Username,
+            Sex = newUser.Sex,
+            Password = newUser.Password,
+            Email = newUser.Email,
+            PhoneNumber = newUser.PhoneNumber
+        };
+        
+        try
+        {
+            await userService.AddUser(user);
+            return Ok();
+        }
+        catch (Exception e)
+        {
+            return Problem(e.Message);
+        }
+    }
+
+    [HttpPut("users/{id}/update")]
+    public async Task<IActionResult> Update([FromBody] User user, long id)
+    {
+        try
+        {
+            await userService.UpdateUser(user, id);
+            return Ok();
+        }
+        catch (KeyNotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+        catch (Exception e)
+        {
+            return Problem(e.Message);
+        }
+    }
+
+    [HttpDelete("users/{id}/delete")]
+    public async Task<IActionResult> Delete(long id)
+    {
+        try
+        {
+            await userService.DeleteUser(id);
+            return Ok();
+        }
+        catch (KeyNotFoundException e)
+        {
+            return NotFound(e.Message);
+        }
+        catch (Exception e)
+        {
+            return Problem(e.Message);
+        }
+    }
+}
