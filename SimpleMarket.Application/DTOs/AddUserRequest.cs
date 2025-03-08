2381173e614d97b3d8508c9e26e7b5d2fd1be356
Yauheni Shumsky <shumsky.yauheni@gmail.com>
2025-03-08 21:57:25 +0300

    add user DTO

---
+++ SimpleMarket.Application/DTOs/AddUserRequest.cs
@@ -0,0 +1,13 @@
+﻿using SimpleMarket.Core.Enums;
+
+namespace SimpleMarket.Application.DTOs;
+
+public class AddUserRequest
+{
+    public Role Role { get; set; } = Role.User;
+    public string Username { get; set; } = string.Empty;
+    public string Sex { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+}
